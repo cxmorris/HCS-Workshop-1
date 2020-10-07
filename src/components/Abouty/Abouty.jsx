750823e3eef4869c37c8b1771686b72bf8a18eaf
cxmorris <66590011+cxmorris@users.noreply.github.com>
2020-10-07 11:08:34 -0400

    Update Abouty.jsx

--- src/components/Abouty/Abouty.jsx
+++ src/components/Abouty/Abouty.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import React, { useState } from 'react';
 import "./Abouty.css";
 import { Link } from "react-router-dom";
 
 export default function Abouty(props) {
   return (
+    <div>
+      <p> You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      </div>
     <Link 
       className = "abouty-background"
       to = {{ pathname: props.link }}
