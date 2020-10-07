ee20dafe96e75be23a25df993438dd636ec01e65
cxmorris <66590011+cxmorris@users.noreply.github.com>
2020-10-07 10:54:00 -0400

    Update About.jsx

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,19 +1,49 @@
 import React from "react";
 import "./About.css";
-import CutePic from "../../assets/bighappy.png";
+import CutePic from "../../assets/bighappy.jpg";
+import Abouty from "../../components/Abouty/Abouty";
+  
 
 export default function AboutScreen() {
   return (
     <div className="screen-background">
       <div className="intro-background">
+        
+          <img src={CutePic} alt="profile picture" className = "intro-picture"/>
+        
         <div className="intro-item">
-          <div>here you'll put a picture of yourself</div>
-        </div>
-        <div className="intro-item">
-          Wow wow wow Your Name is a pretty cool person! check out all these
-          details
-        </div>
+          <h1>
+          CS @ Harvard, 2024
+          </h1>
+            <p>I am currently involved with Women Engineers Code,   
+            Harvard Computer Society, Harvard Women in Computer Science and 
+            Harvard Open Data Project. I also worked as a 
+            research fellow this past summer in Harvard's Earth and Planetary Sciences 
+            department developing graphical models of climate data in Python.
+            </p>
+
+            <p>I graduated from Oliver M. Hazen High School in June 2020. 
+              While there, I founded a Girls Who Code chapter, 
+              captained the school's cross country team for two consective years, served as a section leader in the school 
+              band program, and served the community as part of the National Honor Society.
+            </p>
+          
+          
+            <p>Outside of CS, I am very involved in classical music!
+              I play the French horn and absolutely love symphony orchestra.
+              In fact, I almost went to music conservatory instead of university-
+              I made it to the final auditions
+              at The Juilliard School, yet ultimately decided a career in tech 
+              would be more fulfilling for its wide impact and ability to help solve
+              global issues.
+            </p>
+          </div>
       </div>
+      <Abouty
+          title = "Music Recordings"
+          description = "Check out some clips of my horn playing here!"
+          link = "https://www.dropbox.com/sh/fbekhtjzp9kfrto/AAA9Iu-Bx5A14M6Fl7MRehpXa?dl=0"
+          />
     </div>
   );
 }
