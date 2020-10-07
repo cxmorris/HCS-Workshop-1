d6a30a7a334088076d62c3591451c4f193acfb3d
cxmorris <66590011+cxmorris@users.noreply.github.com>
2020-10-07 10:52:59 -0400

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,55 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <>
       <div className="info">
-        <h1 style={{ margin: 0 }}>your name</h1>
+        <h1 style={{ margin: 0 }}>Clare Morris</h1>
         <div className="info-bar">
-          <p className="info-item">yourname@college.harvard.edu</p>
+          <p className="info-item">cxmorris@college.harvard.edu</p>
         </div>
         <div className="info-bar">
-          links to cool things you have online maybe?
+          <Link 
+            className = "info-link"
+            to={{pathname: "https://linkedin.com/in/clare-morris-23a89017b"}}
+            target="_blank"
+            >
+              LinkedIn
+            </Link>
+
+          <Link 
+            className = "info-link"
+            to={{pathname: "https://github.com/cxmorris"}}
+            target="_blank"
+            >
+              GitHub
+            </Link>
+
+            <Link 
+            className = "info-link"
+            to={{pathname: "https://www.facebook.com/clare.morris.357"}}
+            target="_blank"
+            >
+              Facebook
+            </Link>
+
+            
+
+            
         </div>
       </div>
-      <div className="nav-background">we need functioning links here!</div>
+      <div className="nav-background">
+        <div className="nav-item">
+
+      <Link className="nav-link" to={{pathname: "/about"}}>About</Link>
+      <Link className="nav-link" to={{pathname: "/education"}}>Education</Link>
+      <Link className="nav-link" to={{pathname: "/experience"}}>Experience</Link>
+      <Link className="nav-link" to={{pathname: "/projects"}}>Projects</Link>
+      </div>
+      
+        </div>
     </>
   );
 }
